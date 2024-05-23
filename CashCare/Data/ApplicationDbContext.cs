252397aff6a1f11b73670bf8e0a4619e5aa14d90
Marwen <Marwen.Ben-Salem@begis.de>
2024-05-23 10:40:36 +0200

    Integration of Entity-FrameWork & Connection

---
+++ CashCare/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CashCare.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashCare.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
