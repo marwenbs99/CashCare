30ad647edb2a03dd84a954f1d2dbdbdabba2204b
Marwen <Marwen.Ben-Salem@begis.de>
2024-05-24 12:41:04 +0200

    Working on Views

---
+++ CashCare/Models/Wallet/Wallet.cs
@@ -0,0 +1,10 @@
+﻿namespace CashCare.Models.Wallet
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public IList<Expense> ExpenseListe { get; set; } = new List<Expense>();
+        public IList<Debt> debts { get; set; } = new List<Debt>();
+        public IList<Income> incomes { get; set; } = new List<Income>();
+    }
+}
