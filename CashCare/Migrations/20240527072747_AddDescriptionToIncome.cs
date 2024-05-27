7bd5f6994db8730c629cda85076028789fd5b762
Marwen <Marwen.Ben-Salem@begis.de>
2024-05-27 11:03:54 +0200

    add a new attribute to Income. new Migration, update-database

---
+++ CashCare/Migrations/20240527072747_AddDescriptionToIncome.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToIncome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DataOfRecive",
+                table: "Incomes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataOfRecive",
+                table: "Incomes");
+        }
+    }
+}
--- CashCare/Models/Wallet/Income.cs
+++ CashCare/Models/Wallet/Income.cs
@@ -1,4 +1,5 @@
 ﻿using CashCare.Models.Wallet.Enum;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CashCare.Models.Wallet
@@ -6,11 +7,35 @@ namespace CashCare.Models.Wallet
     public class Income
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The type of income is required")]
         public IncomeType TypeOfIncome { get; set; }
+        [Required(ErrorMessage = "The amount is required")]
         public decimal Amount { get; set; }
+        [Required(ErrorMessage = "Data of recive is required")]
+        public int DataOfRecive { get; set; }
 
         [ForeignKey("Wallet")]
         public int WalletId { get; set; }
         public Wallet Wallet { get; set; }
+
+        public bool Validate()
+        {
+            if (TypeOfIncome.ToString() == "")
+            {
+                return false;
+            }
+
+            if (Amount <= 0)
+            {
+                return false;
+            }
+
+            if (DataOfRecive == default)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
